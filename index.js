b52e20e32d410538581f9e496a70ec01bcad2280
syahrul9088 <39154644+syahrul9088@users.noreply.github.com>
2022-11-12 14:46:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const fetch = require('node-fetch')
 var randomize = require('randomatic');
+const crypto = require('crypto');
 const readline = require('readline-sync');
 const fs = require('fs-extra');
 const getIPRange = require('get-ip-range');
 
-const functionLogin = (randIp, nomor, password) => new Promise((resolve, reject) => {
+const functionSendOtp = (randIp, nomor) => new Promise((resolve, reject) => {
     const bodys = {
-        "user_login":`62${nomor}`,
-        "user_pass":password,
-        "user_email":"",
-        "source":"android",
+        "mobile":`62${nomor}`,
+        "tag":"register",
         "ip":randIp,
         "v":"2.1.2.2",
         "l":"in"
     }
 
-    fetch(`https://api.yoha.pro/api/auth/login`, { 
+    fetch(`https://api.yoha.pro/api/sms/verify-code`, { 
         method: 'POST', 
         body: JSON.stringify(bodys),
         headers: {
@@ -35,17 +34,35 @@ const functionLogin = (randIp, nomor, password) => new Promise((resolve, reject)
    .catch(err => reject(err))
 });
 
-const functionCheckTask = (randIp, token) => new Promise((resolve, reject) => {
-    fetch(`https://api.yoha.pro/api/signs/init?ip=${randIp}&v=2.1.2.2&l=in`, { 
-        method: 'GET', 
+const functionRegister = (randIp, nomor, reffCode, otp) => new Promise((resolve, reject) => {
+    const bodys = {
+        "user_login":`62${nomor}`,
+        "user_pass":"jajang908",
+        "user_pass2":"jajang908",
+        "source":"android",
+        "referral_code":reffCode,
+        "channel_code":"8",
+        "unique_code":`${randomize('0', 13)}-${randomize('0', 19)}`,
+        "code":otp,
+        "pasteboard":"",
+        "device_code":`${crypto.randomBytes(16).toString('hex')}`,
+        "guest_code":"",
+        "ip":randIp,
+        "v":"2.1.2.2",
+        "l":"in"
+    }
+
+    fetch(`https://api.yoha.pro/api/auth/register`, { 
+        method: 'POST', 
+        body: JSON.stringify(bodys),
         headers: {
             'X-Forwarded-For': `${randIp}`,
-            'Authorization': `Bearer ${token}`,
-            'Accept': 'application/json',
             'Host': 'api.yoha.pro',
-            'Connection': 'Keep-Alive',
-            'Accept-Encoding': 'gzip',
-            'User-Agent': 'okhttp/5.0.0-alpha.2'
+            'accept': 'application/json',
+            'content-type': 'application/json; charset=utf-8',
+            'content-length': '333',
+            'accept-encoding': 'gzip',
+            'user-agent': 'okhttp/5.0.0-alpha.2'
         }
    })
    .then(res => res.json())
@@ -55,26 +72,28 @@ const functionCheckTask = (randIp, token) => new Promise((resolve, reject) => {
    .catch(err => reject(err))
 });
 
-const functionClaim = (randIp, totalClaim, token) => new Promise((resolve, reject) => {
+const functionLogin = (randIp, nomor) => new Promise((resolve, reject) => {
     const bodys = {
+        "user_login":`62${nomor}`,
+        "user_pass":"jajang908",
+        "user_email":"",
+        "source":"android",
         "ip":randIp,
         "v":"2.1.2.2",
         "l":"in"
     }
 
-    fetch(`https://api.yoha.pro/api/signs/store?day=${totalClaim}&sign_id=${totalClaim}&ip=${randIp}&v=2.1.2.2&l=in`, { 
+    fetch(`https://api.yoha.pro/api/auth/login`, { 
         method: 'POST', 
         body: JSON.stringify(bodys),
         headers: {
             'X-Forwarded-For': `${randIp}`,
-            'Authorization': `Bearer ${token}`,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json; charset=utf-8',
-            'Content-Length': 47,
             'Host': 'api.yoha.pro',
-            'Connection': 'Keep-Alive',
-            'Accept-Encoding': 'gzip',
-            'User-Agent': 'okhttp/5.0.0-alpha.2'
+            'accept': 'application/json',
+            'content-type': 'application/json; charset=utf-8',
+            'content-length': 87,
+            'accept-encoding': 'gzip',
+            'user-agent': 'okhttp/5.0.0-alpha.2'
         }
    })
    .then(res => res.json())
@@ -148,59 +167,127 @@ const functionSendGift = (randIp, token, streamId, liveId) => new Promise((resol
    .catch(err => reject(err))
 });
 
+const functionCheckTask = (randIp, token) => new Promise((resolve, reject) => {
+    fetch(`https://api.yoha.pro/api/signs/init?ip=${randIp}&v=2.1.2.2&l=in`, { 
+        method: 'GET', 
+        headers: {
+            'X-Forwarded-For': `${randIp}`,
+            'Authorization': `Bearer ${token}`,
+            'Accept': 'application/json',
+            'Host': 'api.yoha.pro',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip',
+            'User-Agent': 'okhttp/5.0.0-alpha.2'
+        }
+   })
+   .then(res => res.json())
+   .then(result => {
+       resolve(result);
+   })
+   .catch(err => reject(err))
+});
+
+const functionClaim = (randIp, totalClaim, token) => new Promise((resolve, reject) => {
+    const bodys = {
+        "ip":randIp,
+        "v":"2.1.2.2",
+        "l":"in"
+    }
+
+    fetch(`https://api.yoha.pro/api/signs/store?day=${totalClaim}&sign_id=${totalClaim}&ip=${randIp}&v=2.1.2.2&l=in`, { 
+        method: 'POST', 
+        body: JSON.stringify(bodys),
+        headers: {
+            'X-Forwarded-For': `${randIp}`,
+            'Authorization': `Bearer ${token}`,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=utf-8',
+            'Content-Length': 47,
+            'Host': 'api.yoha.pro',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip',
+            'User-Agent': 'okhttp/5.0.0-alpha.2'
+        }
+   })
+   .then(res => res.json())
+   .then(result => {
+       resolve(result);
+   })
+   .catch(err => reject(err))
+});
+
 (async () => {
-    try {
-        const file = readline.question('Nama file (ex: listNo.txt): ')
+    var reffCode = readline.question("Kode reff : ")
+    var jmlReff = readline.question("Jumlah reff: ")
 
-        const listNo = await fs.readFile(`./${file}`, "utf-8")
-        const toArray = listNo.split('\r\n')
-        for(var i in toArray){
+    console.log("")
 
+    for(var i = 0; i < jmlReff; i++){
+        try {
             const listIp = await JSON.parse(fs.readFileSync('./ip.json'))  
             const chooseIp = listIp[Math.floor(Math.random()*listIp.length)]
             const start = chooseIp.start
             const end = chooseIp.end
             const genIp = getIPRange(`${start}`, `${end}`);
             const randIp = genIp[Math.floor(Math.random()*genIp.length)]
+            const nomor = readline.question('Nomor (ex: 819XXXX): ')
 
-            const nomor = toArray[i].split('|')[0]
-            const password = toArray[i].split('|')[1]
+            console.log(`Mencoba regist 0${nomor} => IP address ${randIp}`)
+    
+            const sendOtp = await functionSendOtp(randIp, nomor)
+            if(sendOtp.status == 'success'){
+                console.log(`OTP berhasil dikirim`)
+                const otp = readline.question("OTP : ")
+                const register = await functionRegister(randIp, nomor, reffCode, otp)
+                if(register.status == 'success'){
+                    console.log("Berhasil mendaftar")
+                    const login = await functionLogin(randIp, nomor)
+                    if(login.code == 200){
+                        console.log("Login sukses")
+                        const token = login.data.access_token
 
-            console.log(`Mencoba login ${nomor}|${password} => IP address ${randIp}`)
+                        const checkTask = await functionCheckTask(randIp, token)
+                        const totalClaim = checkTask.data.toDayStatus + 1
+                        console.log(`Reward yang telah diclaim : ${checkTask.data.toDayStatus} hari`)
+                        const claim = await functionClaim(randIp, totalClaim, token)
 
-            const login = await functionLogin(randIp, nomor, password)
-            if(login.code == 200){
-                console.log("Login sukses")
-                const token = login.data.access_token
-                const checkTask = await functionCheckTask(randIp, token)
-                const totalClaim = checkTask.data.toDayStatus + 1
-                console.log(`Reward yang telah diclaim : ${checkTask.data.toDayStatus} hari`)
-                const claim = await functionClaim(randIp, totalClaim, token)
-                if(checkTask.code == 200 && claim.code == 200){
-                    console.log(`Claim harian sukses, reward : ${claim.data.amount}`)
-                    const getUser = await functionGetDetailUserLive(randIp, token)
-                    var randomUser = getUser.data.list[Math.floor(Math.random()*getUser.data.list.length)]
-                    const streamId = randomUser.stream
-                    const liveId = randomUser.uid
-        
-                    const sendGift = await functionSendGift(randIp, token, streamId, liveId)
-                    if(sendGift.code == 200){
-                        console.log(`Berhasil send gift ke ${randomUser.user_nicename}`)
-                        console.log("")
+                        if(checkTask.code == 200 && claim.code == 200){
+                            console.log(`Claim harian sukses, reward : ${claim.data.amount}`)
+                            const getUser = await functionGetDetailUserLive(randIp, token)
+                            var randomUser = getUser.data.list[Math.floor(Math.random()*getUser.data.list.length)]
+                            const streamId = randomUser.stream
+                            const liveId = randomUser.uid
+    
+                            const sendGift = await functionSendGift(randIp, token, streamId, liveId)
+                            if(sendGift.code == 200){
+                                console.log(`Berhasil send gift ke ${randomUser.user_nicename}`)
+                                await fs.appendFile('./listNo.txt',`${nomor}|jajang908`+'\r\n', err => {
+                                    if (err) throw err;
+                                })
+                                console.log(`Info akun disimpan di listNo.txt`)
+                                console.log("")
+                            } else {
+                                console.log(`Gagal send gift, ${sendGift.message}`)
+                                console.log("")
+                            }
+                        } else {
+                            console.log("CLaim gagal, coba esok hari")
+                            console.log("")
+                        }
                     } else {
-                        console.log(`Gagal send gift, ${sendGift.message}`)
+                        console.log(`Login gagal, ${login.message}`)
                         console.log("")
                     }
                 } else {
-                    console.log("Claim gagal, coba esok hari")
+                    console.log(`Gagal mendaftar, ${register.message}`)
                     console.log("")
                 }
             } else {
-                console.log(`Login gagal, ${login.message}`)
+                console.log(`OTP gagal dikirim, ${sendOtp.message}`)
                 console.log("")
             }
+        } catch (error) {
+            console.log(error);
         }
-    } catch (error) {
-        console.log(error);
     }
 })();
